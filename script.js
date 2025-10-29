0e3d871a20115c5784acede8a2deb4adcec13365
Taha Idriss <mightybalut@gmail.com>
2025-10-29 19:55:53 -0400

    update

---
+++ script.js
@@ -0,0 +1,48 @@
+// IMPORTANT - substitute your own backend url!
+const API_BACKEND_URL = "https://week-4-backend-ashy.vercel.app/";  
+
+const itemName = document.getElementById('inpName');
+const itemDesc = document.getElementById('inpDesc');
+
+const btnAdd = document.getElementById('btnAdd');
+const btnList = document.getElementById('btnList');
+
+const itemList = document.getElementById('lstItems');
+
+
+btnAdd.addEventListener('click', addItem );
+
+btnList.addEventListener('click', listItems );
+
+
+async function addItem() {
+
+    let new_item_name = itemName.value;
+    let new_item_desc = itemDesc.value;
+
+    // could check that name,desc are not blank
+
+    // call the back-end route that adds a record
+    await fetch(`${API_BACKEND_URL}/item`, {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ new_item_name,new_item_desc }),
+    });
+
+}
+
+async function listItems() {
+    // call the backend route that gets all records
+    let resp = await fetch(`${API_BACKEND_URL}/items`);
+    let data = await resp.json();
+    console.log(data);
+
+    itemList.innerHTML = ""; // clear list
+
+    for( item of data ) {
+        let li = document.createElement('li');
+        li.textContent = item.item_name + ' - ' + item.item_desc;
+        itemList.appendChild(li);
+    }
+
+}
